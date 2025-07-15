84eec1e31928de297c168cfc49629b20fdaca204
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-16 02:39:18 +0700

    addl2tp.sh

--- ipsec/addl2tp.sh
+++ ipsec/addl2tp.sh
@@ -20,7 +20,7 @@ PUBLIC_IP=$(wget -qO- ipinfo.io/ip);
 else
 PUBLIC_IP=$IP
 fi
-source /var/lib/akbarstorevpn/ipvps.conf
+source /var/lib/rakhastorevpn/ipvps.conf
 if [[ "$IP2" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
@@ -28,7 +28,7 @@ domain=$IP2
 fi
 until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Username : " -e VPN_USER
-		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/akbarstorevpn/data-user-l2tp | wc -l)
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/rakhastorevpn/data-user-l2tp | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
@@ -54,7 +54,7 @@ EOF
 
 # Update file attributes
 chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
-echo -e "### $VPN_USER $exp">>"/var/lib/akbarstorevpn/data-user-l2tp"
+echo -e "### $VPN_USER $exp">>"/var/lib/rakhastorevpn/data-user-l2tp"
 cat <<EOF
 
 ============================
