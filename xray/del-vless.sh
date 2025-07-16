c0a293219182e2e6d1d8ed3d92a63e842422409a
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-16 14:02:50 +0700

    del-vless.sh

--- xray/del-vless.sh
+++ xray/del-vless.sh
@@ -5,23 +5,23 @@ clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m     ⇱ Delete Vless Account ⇲      \E[0m"
+echo -e "\E[44;1;39m     ⇱ Hapus Akun Vless ⇲      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "  • You Dont have any existing clients!"
+echo -e "  • anda tidak memiliki pengguna trojan!"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
+read -n 1 -s -r -p "   tekan apasaja untuk kembali ke menu"
 m-vless
 fi
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m     ⇱ Delete Vless Account ⇲      \E[0m"
+echo -e "\E[44;1;39m     ⇱ Hapus Akun Vless ⇲      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 grep -E "^#& " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq | nl
 echo -e ""
-echo -e "  • [NOTE] Press any key to back on menu"
+echo -e "  • [NOTE] Tekan apa saja untuk kembali ke menu"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -rp "   Input Username : " user
+read -rp "   Nama Pengguna : " user
 if [ -z $user ]; then
 m-vless
 else
@@ -30,14 +30,14 @@ sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
 systemctl restart xray > /dev/null 2>&1
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m     ⇱ Delete Vless Account ⇲      \E[0m"
+echo -e "\E[44;1;39m     ⇱ Hapus Akun Vless ⇲      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "   • Accound Delete Successfully"
+echo -e "   • Pengguna Berhasil Dihapus"
 echo -e ""
-echo -e "   • Client Name : $user"
-echo -e "   • Expired On  : $exp"
+echo -e "   • Nama Pengguna : $user"
+echo -e "   • Berakhir sampai  : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-read -n 1 -s -r -p "   Press any key to back on menu"
+read -n 1 -s -r -p "   tekan apasaja untuk kembali ke menu"
 m-vless
 fi
