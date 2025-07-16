159e3561b6c946f2db1907ac16c1296454e0255c
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-16 15:06:47 +0700

    menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -54,7 +54,7 @@ dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($
 umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
 # user
-Exp2=$"seumur hidup"
+Exp2=$"life time"
 Name=$"RAKHA-VPN"
 # Getting CPU Information
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
@@ -102,20 +102,18 @@ echo -e "\e[1;36m 2 \e[0m: Menu Vmess"
 echo -e "\e[1;36m 3 \e[0m: Menu Vless"
 echo -e "\e[1;36m 4 \e[0m: Menu Trojan"
 echo -e "\e[1;36m 5 \e[0m: Menu Shadowsocks"
-echo -e "\e[1;36m 6 \e[0m: Menu L2TP"
-echo -e "\e[1;36m 7 \e[0m: Menu PPTP"
-echo -e "\e[1;36m 8 \e[0m: Menu Setting"
-echo -e "\e[1;36m 9 \e[0m: Status Service"
-echo -e "\e[1;36m 10 \e[0m: Clear RAM Cache"
-echo -e "\e[1;36m 11 \e[0m: Reboot VPS"
+echo -e "\e[1;36m 6 \e[0m: Menu Setting"
+echo -e "\e[1;36m 7 \e[0m: Status Service"
+echo -e "\e[1;36m 8 \e[0m: Clear RAM Cache"
+echo -e "\e[1;36m 9 \e[0m: Reboot VPS"
 echo -e "\e[1;36m x \e[0m: Exit Script"
 echo -e   ""
 echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m Nama Pengguna \e[0m: $Name"
+echo -e "\e[1;32m Nama Pengguna    \e[0m: $Name"
 echo -e "\e[1;32m Masa Berlaku     \e[0m: $Exp2"
 echo -e "\e[1;33m -------------------------------------------------\e[0m"
 echo -e   ""
-echo -e "\e[1;36m ----------------RAKHA PUTRA ANDRIAN---------------\e[0m"
+echo -e "\e[1;36m ----------------0821 3007 7713---------------\e[0m"
 echo -e   ""
 read -p " Pilih Menu :  "  opt
 echo -e   ""
@@ -125,12 +123,10 @@ case $opt in
 3) clear ; m-vless ;;
 4) clear ; m-trojan ;;
 5) clear ; m-ssws ;;
-6) clear ; m-l2tp ;;
-7) clear ; m-pptp ;;
-8) clear ; m-system ;;
-9) clear ; running ;;
-10) clear ; clearcache ;;
-11) clear ; reboot ; /sbin/reboot ;;
+6) clear ; m-system ;;
+7) clear ; running ;;
+8) clear ; clearcache ;;
+9) clear ; reboot ; /sbin/reboot ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu ;;
 esac
