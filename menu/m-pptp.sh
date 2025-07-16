c51ec31210a13bfb870da2280f92f69479eb8649
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-16 13:37:30 +0700

    m-pptp.sh

--- menu/m-pptp.sh
+++ menu/m-pptp.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+echo "Checking VPS"
 clear
-m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
-yl="\033[0;1;33m"
-wh="\033[0m"
 echo -e "$y                             PPTP $wh"
 echo -e "$y-------------------------------------------------------------$wh"
 echo -e "$yy 1$y. Create Account PPTP"
