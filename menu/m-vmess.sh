29b1483e5f5de47c4e77705c107fd3b340b36990
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-18 13:24:34 +0700

    m-vmess.sh

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -10,15 +10,15 @@ echo -e " [\e[36m•1\e[0m] Buat Akun Vmess "
 echo -e " [\e[36m•2\e[0m] Percobaan Akun Vmess "
 echo -e " [\e[36m•4\e[0m] Hapus Akun Vmess "
 echo -e " [\e[36m•5\e[0m] Cek Pengguna Masuk Vmess "
-echo -e " [\e[36m•6\e[0m] Daftar Pengguna Buat Akun "
+echo -e " [\e[36m•6\e[0m] Daftar Pengguna Vmess "
 echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e " [\e[31m•0\e[0m] \e[31mKEMBALI KE MENU\033[0m"
 echo -e ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo -e   "tekan x atau [ Ctrl+C ] • untuk keluar"
 echo ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-read -p " Select menu :  "  opt
+read -p " pilih menu :  "  opt
 echo -e ""
 case $opt in
 1) clear ; add-ws ; exit ;;
